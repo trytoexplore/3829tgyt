61b61794bb041105e44174ac1b053244e6668782
trytoexplore <97546125+trytoexplore@users.noreply.github.com>
2022-01-11 23:16:41 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -13,7 +13,7 @@ async def verupikkals(bot, message):
     users = await db.get_all_users()
     b_msg = message.reply_to_message
     sts = await message.reply_text(
-        text='Broadcasting your messages...'
+        text='Made By : @LG_Bot_Updates..........Broadcasting your messages...'
     )
     start_time = time.time()
     total_users = await db.total_users_count()
